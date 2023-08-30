34f6c77398cc8ca3275a2d0a804566f6451050da
Maitann <hiagomaitan@gmail.com>
2023-08-30 03:52:12 -0300

    FEAT: add findEventByLocation and function  calculateDistance

--- src/useCases/EventUseCase.ts
+++ src/useCases/EventUseCase.ts
@@ -15,13 +15,12 @@ class EventUseCase {
     if (!eventData.location)
       throw new HttpException(400, 'Location is required')
 
-
     // verificar se existe um evento no mesmo local e horario
     const verifyEvent = await this.eventRepository.findByLocationAndDate(
       eventData.location,
       eventData.date
     );
-    if(verifyEvent) throw new HttpException(400, 'Event already exists')
+    if (verifyEvent) throw new HttpException(400, 'Event already exists')
 
     const cityName = await this.getCityNameByCoordinates(
       eventData.location.latitude,
@@ -34,6 +33,26 @@ class EventUseCase {
 
     const result = await this.eventRepository.add(eventData);
     return result;
+  }
+
+  async findEventByLocation(latitude: string, longitude: string) {
+    const cityName = await this.getCityNameByCoordinates(latitude, longitude)
+
+    const findEventsByCity = await this.eventRepository.findEventsByCity(
+      cityName,
+    );
+
+    const eventWithRadius = findEventsByCity.filter(event => {
+      const distance = this.calculateDistance(
+        Number(latitude),
+        Number(longitude),
+        Number(event.location.latitude),
+        Number(event.location.longitude)
+      )
+      return distance <= 3
+    })
+
+    return eventWithRadius;
   }
 
   private async getCityNameByCoordinates(latitude: string, longitude: string) {
@@ -57,6 +76,29 @@ class EventUseCase {
       throw new HttpException(401, 'Error request city name')
     }
   }
+  private calculateDistance(
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+  ): number {
+    const R = 6371 // Raio da Terra em quilômetros
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) *
+      Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;
+  }
+  private deg2rad(deg: number): number {
+    return deg * (Math.PI / 180);
+  }
+
 }
 
 export { EventUseCase }
