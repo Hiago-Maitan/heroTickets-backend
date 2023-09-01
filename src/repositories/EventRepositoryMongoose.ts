9e88f164e42a05b6b51361074b0c9ce3872c2e34
Maitann <hiagomaitan@gmail.com>
2023-09-01 05:42:02 -0300

    FEAT: add new findEvents methods

--- src/repositories/EventRepositoryMongoose.ts
+++ src/repositories/EventRepositoryMongoose.ts
@@ -42,12 +42,36 @@ class EventRepositoryMongoose implements EventRepository {
     date: Date,
   ): Promise<Event | undefined> {
     const findEvent = await EventModel.findOne({ location, date }).exec()
-   
+
     return findEvent ? findEvent.toObject() : undefined;
   }
-  async findEventsByCity(city: string): Promise<Event[]>{
+  async findEventById(id: string): Promise<Event | undefined> {
+    const findEvent = await EventModel.findOne({ _id: id }).exec()
+
+    return findEvent ? findEvent.toObject() : undefined;
+  }
+  async findEventsByCity(city: string): Promise<Event[]> {
     const findEvent = await EventModel.find({ city }).exec()
-   
+
+    return findEvent.map((event => event.toObject()))
+  }
+  async findEventsByCategory(category: string): Promise<Event[]> {
+    const findEvent = await EventModel.find({ categories: category }).exec()
+
+    return findEvent.map((event => event.toObject()))
+  }
+  async update(event: Event, id: string): Promise<any> {
+    const eventUpdate = await EventModel.updateMany({ _id: id }, event);
+    return eventUpdate;
+  }
+  async findEventsByName(name: string): Promise<Event[]> {
+    const findEvent = await EventModel.find({
+      title: {
+        $regex: name,
+        $options: 'i'
+      }
+    }).exec()
+
     return findEvent.map((event => event.toObject()))
   }
 }
