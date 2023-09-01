bf88b541ccc54556b8b4d017f92dd3c9d135a225
Maitann <hiagomaitan@gmail.com>
2023-09-01 05:35:57 -0300

    FEAT: add methods findEvents and addParticipants

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -29,13 +29,75 @@ class EventController {
       next(error);
     }
   }
-  async findEventByLocation(request: Request, response: Response, next: NextFunction){
-    const {latitude, longitude} = request.query
+  async findEventByLocation(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { latitude, longitude } = request.query
     try {
       const events = await this.eventUseCase.findEventByLocation(
         String(latitude),
         String(longitude)
       )
+      return response.status(200).json(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async findEventsByCategory(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { category } = request.params
+    try {
+      const events = await this.eventUseCase.findEventsByCategory(
+        String(category),
+      )
+      return response.status(200).json(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async findEventsByName(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { name } = request.query;
+
+    try {
+      const events = await this.eventUseCase.findEventsByName(String(name));
+      return response.status(200).json(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async findEventById(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { id } = request.params;
+
+    try {
+      const events = await this.eventUseCase.findEventById(String(id));
+      return response.status(200).json(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async addParticipant(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const { name, email } = request.body
+    const { id } = request.params;
+
+    try {
+      const events = await this.eventUseCase.addParticipant(id, name, email);
       return response.status(200).json(events)
     } catch (error) {
       next(error)
